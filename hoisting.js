7402e25eff075919fb308669eb428eda4ac39702
Enrique Hernandez <enrique_inf@hotmail.com>
2023-03-09 23:30:20 -0600

    agregando hoisting

---
+++ hoisting.js
@@ -0,0 +1,7 @@
+//El hoisting solo sucede en las versiones previas a js 5
+// Esto se soluciona con las palabras reservedas const y let
+
+// La palabra clave var si provoca hoisting
+
+var miNombre = "Quique";
+const primerNombre = "Luis";
